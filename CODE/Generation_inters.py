e7d97322b0d86cab4b3e3d76b1aa7beb903f7a99
Cobaye37 <43568421+Cobaye37@users.noreply.github.com>
2019-03-20 10:34:38 +0100

    Update Generation_inters.py

--- CODE/Generation_inters.py
+++ CODE/Generation_inters.py
@@ -1,9 +1,14 @@
 # 20 interventions
-inters = ["accident routier sur autoroute", "cambriolage", "voiture mal stationee", "tentative de meurtre", "convoi", "personnne disparue", "feu de foret", "feu en ville", "arret cardique", "blessure en montage", "animal coince", "feu en zone maritime (port)", "zone de controle radar", "accident routier non autoroute", "blessure/malaise", "fuite de gaz", "incident ligne EDF", "arrestation d'un suspect", "incident chimique/nucleaire"]
+#inters = ["accident routier sur autoroute", "cambriolage", "voiture mal stationee", "tentative de meurtre", "convoi", "personnne disparue", "feu de foret", "feu en ville", "arret cardique", "blessure en montage", "animal coince", "feu en zone maritime (port)", "zone de controle radar", "accident routier non autoroute", "blessure/malaise", "fuite de gaz", "incident ligne EDF", "arrestation d'un suspect", "incident chimique/nucleaire"]
 import random
-numero_inter = random.randint(0, (len (inters)-1))
 
-def generate_inters() : 
+def generate_inters() :
+    
+    inters = ["accident routier sur autoroute", "cambriolage", "voiture mal stationee", "tentative de meurtre", "convoi", "personnne disparue", "feu de foret", "feu en ville", "arret cardique", "blessure en montage", "animal coince", "feu en zone maritime (port)", "zone de controle radar", "accident routier non autoroute", "blessure/malaise", "fuite de gaz", "incident ligne EDF", "arrestation d'un suspect", "incident chimique/nucleaire"]
+    intervention = []
+    
+    numero_inter = random.randint(0, (len (inters)-1))
+    
     return inters[numero_inter] 
 
 print(generate_inters())
