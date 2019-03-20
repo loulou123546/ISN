1a058f217df32931dd84e843c90cf5ba1e1c613d
Cobaye37 <43568421+Cobaye37@users.noreply.github.com>
2019-03-20 10:30:15 +0100

    Update Generation_inters.py

--- CODE/Generation_inters.py
+++ CODE/Generation_inters.py
@@ -1,7 +1,7 @@
 # 20 interventions
 inters = ["accident routier sur autoroute", "cambriolage", "voiture mal stationee", "tentative de meurtre", "convoi", "personnne disparue", "feu de foret", "feu en ville", "arret cardique", "blessure en montage", "animal coince", "feu en zone maritime (port)", "zone de controle radar", "accident routier non autoroute", "blessure/malaise", "fuite de gaz", "incident ligne EDF", "arrestation d'un suspect", "incident chimique/nucleaire"]
 import random
-numero_inter = random.randint(0, len (inters))
+numero_inter = random.randint(0, (len (inters)-1))
 
 def generate_inters() : 
     return inters[numero_inter] 
