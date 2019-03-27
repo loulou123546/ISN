19fcfe96e229aaafcf76eee9337d91987c4617a7
Cobaye37 <43568421+Cobaye37@users.noreply.github.com>
2019-03-27 10:13:39 +0100

    Add files via upload

---
+++ CODE/vehicules_bloc_1.py
@@ -0,0 +1,92 @@
+DEPANNEUSE1 = pygame.image.load("vehicules\\depanneuse1.png")
+DEPANNEUSE1rect = DEPANNEUSE1.get_rect()
+
+DEPANNEUSE2 = pygame.image.load("vehicules\\depanneuse2.png")
+DEPANNEUSE2rect = DEPANNEUSE2.get_rect()
+
+PATROUILLEUR = pygame.image.load("vehicules\\patrouilleur2.png")
+PATROUILLEURrect = PATROUILLEUR.get_rect()
+
+UMH1 = pygame.image.load("vehicules\\UMH1.png")
+UMH1rect = IMH1.get_rect()
+
+UMH2 = pygame.image.load("vehicules\\UMH1.png")
+UMH2rect = IMH2.get_rect()
+
+VSAV1 = pygame.image.load("vehicules\\VSAV2.png")
+VSAV1rect = VSAV1.get_rect() 
+
+VSAV2 = pygame.image.load("vehicules\\VSAV2.png")
+VSAV2rect = VSAV2.get_rect() 
+
+VSR = pygame.image.load("vehicules\\VSR1.png")
+VSRrect = VSR.get_rect() 
+
+FPTSR = pygame.image.load("vehicules\\FPTSR1.png")
+FPTSRrect = FPTSR.get_rect()
+
+VLPOMPIER = pygame.image.load("vehicules\\VLHR_pompier1.png")
+VLPOMPIERrect = VLPOMPIER.get_rect()
+
+VLPOLICE1 = pygame.image.load("vehicules\\VL_police1.png")
+VLPOLICE1rect = VLPOLICE1.get_rect()
+
+VLPOLICE2 = pygame.image.load("vehicules\\VL_police1.png")
+VLPOLICE2rect = VLPOLICE2.get_rect()
+
+VLPOLICE3 = pygame.image.load("vehicules\\VL_police1.png")
+VLPOLICE3rect = VLPOLICE3.get_rect()
+
+PCM = pygame.image.load("vehicules\\PCM1.png")
+PCMrect = PCM.get_rect()
+
+DRAGON = pygame.image.load("vehicules\\dragon_avec_rotor1.png")
+DRAGONrect = DRAGON.get_rect()
+
+GAITAN = pygame.image.load("vehicules\\gaitan_avec_rotor1.png")
+GAITANrect = GAITAN.get_rect()
+
+VICTOR = pygame.image.load("vehicules\\victor_avec_rotor1.png")
+VICTORrect = VICTOR.get_rect()
+
+VLU = pygame.image.load("vehicules\\undercover1.png")
+VLUrect = VLU.get_rect()
+
+CCF = pygame.image.load("vehicules\\CCF1.png")
+CCFrect = CCF.get_rect()
+
+EPA = pygame.image.load("vehicules\\EPA1.png")
+EPArect = EPA.get_rect()
+
+FPT = pygame.image.load("vehicules\\FPT1.png")
+FPTrect = FPT.get_rect() 
+
+VLM = pygame.image.load("vehicules\\VLM1.png")
+VLMrect = VLM.get_rect()
+
+VTU = pygame.image.load("vehicules\\VTU1.png")
+VTUrect = VTU.get_rect()
+
+CCGC = pygame.image.load("vehicules\\CCGC1.png")
+CCGCrect = CCGC.get_rect()
+
+BRI = pygame.image.load("vehicules\\BRI_police1.png")
+BRIrect = BRI.get_rect()
+
+AMBULANCE = pygame.image.load("vehicules\\ambu_privé1.png")
+AMBULANCErect = AMBULANCE.get_rect()
+
+GERDF = pygame.image.load("vehicules\\VL_GRDF1.png")
+GERDFrect = GERDF.get_rect()
+
+NACELLE = pygame.image.load("vehicules\\ERDF2.png")
+NACELLErect = NACELLE.get_rect()
+
+FOURGON = pygame.image.load("vehicules\\fourgon_prisonnier2.png")
+FOURGONrect = FOURGON.get_rect()
+
+SWAT = pygame.image.load("vehicules\\SWAT_team.png")
+SWATrect = SWAT.get_rect()
+
+NRBC = pygame.image.load("vehicules\\NRBC1.png")
+NRBCrect = NRBC.get_rect()
